5f59c0717a3f5eb8e33e0d5d8afeb30de8010c7f
Julie Evans <juliemevans17@gmail.com>
2021-11-16 17:09:15 -0600

    commit tree fcc7e72f16c2f369fd73c6b82f8fc88aa4ca259b

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const body = document.querySelector("body");
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    const dataContent = document.createElement("p");
+    dataContent.innerHTML = data.id;
+    body.appendChild(dataContent);
+  })
+  .catch(error => {
+    alert("Error Here!");
+    console.log(error.message);
+    const errorContent = document.createElement("p");
+    errorContent.innerHTML = error.message;
+    body.appendChild(errorContent)
+  });
+}
